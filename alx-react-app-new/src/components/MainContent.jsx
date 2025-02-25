b5e4d90b1e0de8beb71dda64f01bf0784960ed6a
Nestor M <ntor2004@gmail.com>
2025-02-25 08:06:06 +0100

    Initialize React app with Vite setup and add core components

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+    return (
+        <main style={{color: 'navy', textAlign: 'center'}}>
+            <p>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+
+export default MainContent;
--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,8 +1,6 @@
 const UserProfile =  (props) => {
     return (
-        <div style={{ border: '1px solid gray', padding: '10px', 
-            margin: '10px'
-        }}>
+        <div>
             <h2>{props.name}</h2>
             <p>Age: {props.age}</p>
             <p>Bio: {props.bio}</p>
