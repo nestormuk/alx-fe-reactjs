479f265a3c16fac230e07bdaae225b9451c7f64d
Nestor M <ntor2004@gmail.com>
2025-02-25 09:58:04 +0100

    Update Navbar component styling and adjust Contact link

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import { Outlet, Link } from 'react-router-dom'
 function Navbar() {
   return (
-    <div>
+    <div style={{backgroundColor: 'white', display: 'flex', justifyContent: 'center'}}>
         <nav>
             <ul>
                 <li><Link to="/" >Home</Link></li>
                 <li><Link to="/about" >About</Link></li>
                 <li><Link to="/service" >Service</Link></li>
-                <li><Link to="/contact" >Contact</Link></li>
+                <li><Link to="/contact" >Contact </Link></li>
             </ul>
         </nav>
         <Outlet />
