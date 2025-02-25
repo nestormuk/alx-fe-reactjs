7214cb89d5bd3525124cc3c97bf9f2ea2d127990
Nestor M <ntor2004@gmail.com>
2025-02-25 09:17:23 +0100

    Refactor UserProfile component to use UserContext for user data management

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,11 +1,14 @@
-const UserProfile =  (props) => {
+import UserContext from "./UserContext";
+
+function UserProfile()  {
+    const userData = useContext(UserContext);
     return (
         <div style={{ border: '1px solid gray', padding: '10px', 
             margin: '10px'
         }}>
-            <h2>{props.name}</h2>
-            <p>Age: {props.age}</p>
-            <p>Bio: {props.bio}</p>
+            <h2>{userData.name}</h2>
+            <p>Email: {userData.email}</p>
+            
         </div>
     );
 }
