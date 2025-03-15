a6305c2e3e4855b6d5a3d8cf48a89ccf7502d903
Nestor M <ntor2004@gmail.com>
2025-03-15 07:30:12 +0100

    Initialize React project with Vite, set up basic structure and add PostsComponent for fetching posts

---
+++ react-query-demo/src/App.jsx
@@ -0,0 +1,15 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import PostsComponent from './components/PostsComponent';
+const queryClient = new QueryClient();
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <h1>React Query Demo</h1>
+      <PostsComponent />
+    </QueryClientProvider>
+  );
+  
+}
+
+export default App
---
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -0,0 +1,41 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from 'axios';
+import React from 'react'
+
+const fetchPosts = async () => {
+    const { data } = await axios.get('https://jsonplaceholder.typicode.com/posts');
+    return data;
+};
+
+
+
+export default function PostsComponent() {
+  
+    const { data, isLoading, error, refetch } = useQuery ({
+        queryKey: ['posts'],
+        queryFn: fetchPosts,
+        staleTime: 5000,
+    });
+
+    if (isLoading) return <p>Loading posts...</p>;
+    if (error) return <p>Error fetching posts: {error.message}</p>;
+
+    return (
+        <div>
+            <h2>Posts</h2>
+            <button onClick={() => refetch()} style={{ marginBottom: '10px'}}>
+                Refresh Posts
+            </button>
+
+            <ul>
+                {data.slice(0,10).map((post) => (
+                    <li key={post.id}>
+                        <strong>{post.title}</strong>
+                        <p>{post.body}</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
