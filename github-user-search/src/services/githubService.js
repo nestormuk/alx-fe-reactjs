b45b224ff08bb735b8d858720ed197ef198d52ca
Nestor M <ntor2004@gmail.com>
2025-04-04 11:53:30 +0200

    Update GitHub API URL to include query parameter for user search

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const GITHUB_API_URL = import.meta.env.VITE_APP_GITHUB_API_URL || "https://api.github.com/search/users";
+const GITHUB_API_URL = import.meta.env.VITE_APP_GITHUB_API_URL || "https://api.github.com/search/users?q";
 
 export const fetchAdvancedUserData = async (username, location, minRepos) => {
   try {
