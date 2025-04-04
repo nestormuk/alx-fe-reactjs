09375ce53c5db06dc35f4ce05b472f78644e53d3
Nestor M <ntor2004@gmail.com>
2025-04-04 11:14:30 +0200

    Update GitHub API URL in environment file and service to use environment variable

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const GITHUB_API_URL = 'https://api.github.com/users';
+const GITHUB_API_URL = import.meta.env.VITE_APP_GITHUB_API_KEY;
 
 export const fetchUserData = async (username) => { 
     try {
